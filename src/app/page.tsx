d3afb159e49a033342ae57881111869d3878ec7f
Carlos <catalacarlos23@gmail.com>
2025-05-06 20:34:11 -0400

    tes?t

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,9 @@
-import FallingNotesBackground from "./components/FallingNotesPackground";
 import ListeningActivity from "./components/ListeningActivity";
 import SocialIcons from "./components/SocialIcons";
 import Top3 from "./components/Top3";
 
 export default function Home() {
   return (
-    <FallingNotesBackground>
       <div className="absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 w-full max-w-3xl px-4 text-center">
         <div className="flex flex-col items-center gap-3 mb-4">
           <p className="text-white text-sm font-light tracking-widest lowercase">hi i&apos;m</p>
@@ -21,6 +19,5 @@ export default function Home() {
 
         <Top3 />
       </div>
-    </FallingNotesBackground>
   );
 }
